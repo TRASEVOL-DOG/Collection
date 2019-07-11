dc20b537478dbcddbd0706176cdede50321228b3
Eliott <eliotts@protonmail.com>
2019-07-11 17:05:45 +0200

    framework link changed

--- framework/framework.lua
+++ framework/framework.lua
@@ -51,8 +51,8 @@
 if CASTLE_PREFETCH then
   CASTLE_PREFETCH({
     "sugarcoat/sugarcoat.lua",
-    "framework/glyphs.png",
-    "framework/HungryPro.ttf"
+    "glyphs.png",
+    "HungryPro.ttf"
   })
 end
 
