0bb407ab80c5f7c26b63748b9dc12b5bbfe9ad05
Eliott <eliotts@protonmail.com>
2019-07-11 15:09:46 +0200

    lol typo

--- game_list.lua
+++ game_list.lua
@@ -1,7 +1,7 @@
 
 _game_list = {
-  {name = "fishing_game", path = "https://raw.githubusercontent.com/TRASEVOL-DOG/Collection/master/fishing_game.lua"}
-  {name = "game_template", path = "https://raw.githubusercontent.com/TRASEVOL-DOG/Collection/master/game_template.lua"}
+  {name = "fishing_game", path = "https://raw.githubusercontent.com/TRASEVOL-DOG/Collection/master/fishing_game.lua"},
+  {name = "game_template", path = "https://raw.githubusercontent.com/TRASEVOL-DOG/Collection/master/game_template.lua"},
   {name = "game_template2", path = "https://raw.githubusercontent.com/TRASEVOL-DOG/Collection/master/game_template2.lua"}
 }
 
