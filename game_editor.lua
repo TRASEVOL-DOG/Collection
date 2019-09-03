703a46100c858a8e0be961d1dce7d3d46a667700
Remy <trasevol.dog@gmail.com>
2019-09-03 15:43:17 +0200

    Game editor: fixing linebreak on generated files + adding comment on generated lua file

--- game_editor.lua
+++ game_editor.lua
@@ -663,11 +663,10 @@ do ---- Game saving + loading
     local thumb = thumbnail_data()
     
     thumb:encode("png", game_info._title.."/preview.png")
-    love.filesystem.write(game_info._title.."/main.lua", "castle.game.load('ll4uzw', { id = '"..game_info._id.."', play = true })")
+    love.filesystem.write(game_info._title.."/main.lua", "-- Launches the game editor with this game's ID to launch it.\r\ncastle.game.load('ll4uzw', { id = '"..game_info._id.."', play = true })")
     love.filesystem.write(game_info._title.."/"..game_info._title..".castle", [[---
 main: main.lua
-title: ]]..game_info._title..[[
-primaryColor: 000000
+title: ]]..game_info._title..[[\r\nprimaryColor: 000000
 coverImage: preview.png
 dimensions: full
 description: ]]..game_info._description)
