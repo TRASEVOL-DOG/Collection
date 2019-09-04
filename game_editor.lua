c6fefd577d269bb25a41e47bb9243efffc01cd1e
Remy <trasevol.dog@gmail.com>
2019-09-04 07:48:09 +0200

    Game editor: allowing uppercase in filenames

--- game_editor.lua
+++ game_editor.lua
@@ -653,7 +653,8 @@ do ---- Game saving + loading
   
   local file_chars = {}
   do
-    local chars = {'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
+    local chars = {
+'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
     for _, k in pairs(chars) do
       file_chars[k] = true
     end
